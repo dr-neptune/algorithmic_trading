fda24913ac4e28c7c4d1c5ed6e55cfc380e948db
dr-neptune <mrose4@worcester.edu>
2022-10-20 08:55:53 -0400

    eh, not great

--- repl.py
+++ repl.py
@@ -73,3 +73,25 @@ class MomentumTrader(tpqoa.tpqoa):
                                                           index=[pd.Timestamp(time)]))
         self.data = self.raw_data.resample(self.bar_length, label='right').last().ffill().iloc[:-1]
         self.data['mid'] = self.data.mean(axis=1)
+        self.data['returns'] = np.log(self.data['mid'] / self.data['mid'].shift(1))
+        self.data['position'] = np.sign(self.data['returns'].rolling(self.momentum).mean())
+
+        if len(self.data) > self.min_length:
+            self.min_length += 1
+            if self.data['position'].iloc[-1] == 1:
+                if self.position == 0:
+                    self.create_order(self.instrument, self.units)
+                elif self.position == -1:
+                    self.create_order(self.instrument, self.units * 2)
+                self.position = 1
+            elif self.data['position'].iloc[-1] == -1:
+                if self.position == 0:
+                    self.create_order(self.instrument, -self.units)
+                elif self.position == 1:
+                    self.create_order(self.instrument, -self.units * 2)
+                self.position = -1
+
+strat = 2
+
+mom = MomentumTrader('pyalgo.cfg', instrument='EUR_USD', bar_length='5s', momentum=6, units=10)
+mom.stream_data(mom.instrument, stop=100)
